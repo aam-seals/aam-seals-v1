c3fa5aca91f03791fada32b86573bc6b4f70a69b
aam-seals <ytzha@umd.edu>
2025-01-03 05:45:19 -0500

    Added loading text

--- static/js/tv-effect.js
+++ static/js/tv-effect.js
@@ -13,7 +13,7 @@
     timeline
       .to(SELECTOR_SCREEN, 0.2, {
         width: '300vw',
-        height: '6px',
+        height: '30px',
         background: '#ffffff',
         ease: Power2.easeOut,
       })
@@ -29,15 +29,6 @@
     // Ensure the timeline starts at the end before reversing
     timeline.progress(1); // Set timeline to its end state
     timeline.reverse(); // Play the animation in reverse (TV "switched on")
-  }
-
-  // Hide the loader and show the content
-  function hideLoaderAndShowContent() {
-    const loader = document.querySelector(SELECTOR_SCREEN);
-    const content = document.querySelector(SELECTOR_CONTENT_INNER);
-
-    if (loader) loader.style.display = 'none';
-    if (content) content.style.display = 'block';
   }
 
   // Automatically start the TV effect and transition to GIF
